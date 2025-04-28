f44ef1651c2672900381f3e8a3cbf7bc2750df41
Trevor Durán <trevor.m.duran@gmail.com>
2025-04-27 21:57:39 -0500

    feat: encrypt with gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 import { src, dest, series } from 'gulp';
 import { deleteAsync as del } from 'del';
 import jsonMinify from 'gulp-json-minify';
+import { readFile, writeFile, unlink } from 'fs/promises';
+import { Buffer } from 'node:buffer';
+import { encode as msgPackEncode } from "@msgpack/msgpack";
 
 const srcDir = 'programs';
 const destDir = 'public';
 
-const encode = series(clean, json);
+const clean = async () => await del(destDir);
 
-async function clean() {
-    await del(destDir);
-}
-
-function json() {
-    return src(`${srcDir}/*.json`)
+const json = () =>
+    src(`${srcDir}/*.json`)
         .pipe(jsonMinify())
         .pipe(dest(destDir));
+
+const base64Encode = async () => {
+    const programsStr = await readFile(`${destDir}/programs.json`);
+    const msgPackBuffer = msgPackEncode(programsStr);
+    const base64Buffer = Buffer.from(msgPackBuffer);
+    let str = '';
+    base64Buffer.forEach((byte) => {
+        str += `${byte} `
+    });
+    str = str.trim();
+
+    await writeFile(`${destDir}/programs.txt`, str);
+    await unlink(`${destDir}/programs.json`);
 }
+
+const encode = series(clean, json, base64Encode);
 
 export { encode as default };
--- src/dataManager.ts
+++ src/dataManager.ts
@@ -8,26 +8,33 @@ export const loadPrograms = async (): Promise<ProgramT[]> => {
     }
     const programsRsp = await fetch('./programs.txt');
     const programsTxt = await programsRsp.text();
+
     return decryptProgramData(programsTxt);
-}
+};
+
+const encryptProgramData = (programs: ProgramT[]) => {
+
+};
 
 export const savePrograms = async (programs: ProgramT[]): Promise<void> => {
-    const programsJson = JSON.stringify(programs);
-    const base64Str = btoa(programsJson);
-    const buffer = encode(base64Str);
-    let str = '';
-    buffer.forEach((byte) => {
-        str += `${byte} `
-    });
-    str = str.trim();
-    window.localStorage.setItem('programs', str);
-}
+    const programData = encryptProgramData(programs);
+    window.localStorage.setItem('programs', programData);
+};
 
 const decryptProgramData = (programsTxt: string): ProgramT[] => {
+    console.group('decrypt');
+    console.log('programsTxt', programsTxt);
     const arr = programsTxt.split(' ').map(char => Number.parseInt(char));
+    console.log('arr', arr);
     const uInt8Array = Uint8Array.from(arr);
-    const base64Str = decode(uInt8Array) as string;
-    const programsJson = atob(base64Str);
+    console.log('uInt8Array', uInt8Array);
+    const base64Buff = decode(uInt8Array) as Uint8Array;
+    console.log('base64Buff', base64Buff);
+    const base64Str = String.fromCharCode(...base64Buff);
+    console.log('base64Str', base64Str);
+    const programs = JSON.parse(base64Str);
+    console.log('programs', programs);
+    console.groupEnd()
 
-    return JSON.parse(programsJson);
-}
+    return programs;
+};
