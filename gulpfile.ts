16c9e8023720e776581cf1e2021576342039e580
Trevor Durán <trevor.m.duran@gmail.com>
2025-05-02 17:22:19 -0500

    feat: msgpack encoding as a custom gulp plugin

--- gulpfile.ts
+++ gulpfile.ts
@@ -3,9 +3,12 @@ import { deleteAsync as del } from 'del';
 import jsonMinify from 'gulp-json-minify';
 import jsonValidator from 'gulp-json-validator';
 import jsonSchema from "gulp-json-schema";
+import { Transform } from 'node:stream';
 import { Buffer } from 'node:buffer';
 import { encode as msgPackEncode } from "@msgpack/msgpack";
-import { Transform } from 'node:stream';
+import PluginError from 'plugin-error';
+import Vinyl from 'vinyl';
+import notify from 'gulp-notify';
 
 const srcDir = 'programs';
 const destDir = 'public';
@@ -22,16 +25,12 @@ const clean = async () => await del(destDir);
  * @param {string} file.path - The file path.
  * @returns {any} The modified file object with updated contents, path, and a `modified` flag.
  */
-const encodeObjectToString = (file: any): any => () => {
-    const programsStr = file.contents.toString();
-    const programs = JSON.parse(programsStr);
+const convertJsonToMsgPack = (jsonStr: string): string => {
+    const programs = JSON.parse(jsonStr);
     const encodedArray = msgPackEncode(programs);
     const encodedString = Array.from(encodedArray).map(byte => byte.toString()).join(' ');
 
-    file.contents = Buffer.from(encodedString);
-    file.path = file.path.replace(/\.json$/, '.txt');
-    file.modified = true;
-    return file;
+    return encodedString;
 };
 
 
@@ -42,29 +41,50 @@ const encodeObjectToString = (file: any): any => () => {
  * @function
  * @returns {Transform} A Transform stream operating in object mode.
  *
- * @example
- * const encodeStream = streamEncode();
- * readableStream.pipe(encodeStream).pipe(writableStream);
  */
-const streamEncode = (): Transform => {
-    const transformStream = new Transform({
+const msgPack = (): Transform => {
+    return new Transform({
         objectMode: true,
-        transform: function (file, _, callback) {
-            let error = null, output = encodeObjectToString(file);
-            callback(error, output);
+        transform(file: Vinyl, encoding, callback) {
+            if (file.isStream()) {
+                return callback(new PluginError('msgPackEncode', 'Streaming not supported'));
+            }
+
+            if (file.isBuffer()) {
+                try {
+                    const contents = file.contents.toString(encoding);
+                    const encodedContents = convertJsonToMsgPack(contents);
+                    file.contents = Buffer.from(encodedContents);
+                    file.path = file.path.replace('.json', '.txt');
+                    file.modified = true;
+                    callback(null, file);
+                } catch (err) {
+                    callback(new PluginError('gulp-msgpack-encode', err as Error));
+                }
+            }
         }
     });
-
-    return transformStream;
 }
 
-const json = () =>
-    src(`${srcDir}/*.json`)
+/**
+ * Processes JSON files from the source directory by validating,
+ * minifying, and transforming them according to a specified schema
+ * The processed files are then encoded using MessagePack and saved
+ * as .txt files in the destination directory. Any errors encountered
+ * in the process are handled with notifications.
+ */
+function json() {
+    return src(`${srcDir}/*.json`)
         .pipe(jsonValidator())
         .pipe(jsonMinify())
         .pipe(jsonSchema("schema.json"))
-        .pipe(streamEncode())
+        .pipe(msgPack())
+        .on('error', notify.onError({
+            title: '<%= error.plugin %>',
+            message: "Error: <%= error.message %>",
+        }))
         .pipe(dest(destDir));
+}
 
 const encode = series(clean, json);
 
