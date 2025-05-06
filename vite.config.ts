3ca86e1f7f74885f5d0802e9d74a1bc086a3d24e
Trevor Durán <trevclev0@gmail.com>
2025-05-05 21:39:36 -0500

    fix: resolve merge conflict for vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 import type { UserConfig } from 'vite'
 
 export default {
-<<<<<<< HEAD
-  appType: 'spa',
-  base: './',
-  server: {
-    open: '/index.html'
-  },
-  build: {
-    target: 'esnext',
-  },
-=======
     appType: 'spa',
     base: './',
     server: {
@@ -19,5 +9,4 @@ export default {
     build: {
       target: 'esnext',
     },
->>>>>>> main
 } satisfies UserConfig
