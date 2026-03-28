f5d459d29368a968c1eb6f3f3135246c171d0ab7
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-28 15:47:55 -0500

    feat: render the program's riddles as neede

--- src/components/Program.tsx
+++ src/components/Program.tsx
@@ -7,10 +7,22 @@ type ProgramProps = {
 };
 
 function Program({ program }: ProgramProps) {
+  // Find the index of the first riddle which is not unlocked
+  const nextRiddleIndex = program.riddles.findIndex(r => !r.unlocked);
+  // The riddles to render are all the riddles which are unlocked, plus the next riddle
+  const riddlesToRender = program.riddles.filter((_, index) =>
+    index < nextRiddleIndex || nextRiddleIndex === -1
+  );
+
+  // Add the current active riddle if one exists
+  if (nextRiddleIndex !== -1) {
+    riddlesToRender.push(program.riddles[nextRiddleIndex]);
+  }
+
   return (
     <>
       <h1 className='title'>{program.name}</h1>
-      {program.riddles.map(riddle => <Riddle key={riddle.id} riddle={riddle} />)}
+      {riddlesToRender.map(riddle => <Riddle key={riddle.id} riddle={riddle} />)}
     </>
   );
 }
