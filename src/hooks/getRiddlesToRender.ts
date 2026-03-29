e6844211dba981e7108f2870b3a0064930cc7c30
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-29 10:44:12 -0500

    refactor: remove return type for getRiddlesToRender

--- src/hooks/getRiddlesToRender.ts
+++ src/hooks/getRiddlesToRender.ts
@@ -1,6 +1,6 @@
 import { RiddleT } from "../types";
 
-function getRiddlesToRender(riddles: RiddleT[]): { riddlesToRender: RiddleT[], nextRiddleIndex: number } {
+function getRiddlesToRender(riddles: RiddleT[]) {
     const nextRiddleIndex = riddles.findIndex(r => !r.unlocked);
     const riddlesToRender = riddles.filter((_, index) =>
         index < nextRiddleIndex || nextRiddleIndex === -1
