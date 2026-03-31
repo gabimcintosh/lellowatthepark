b90f85f4246c8f1674af08f4b077065f4def17dd
Trevor Durán <trevor.m.duran@gmail.com>
2026-03-30 20:45:57 -0500

    fix: remove duplicate riddle to solve

--- src/utils/getRiddlesToRender.ts
+++ src/utils/getRiddlesToRender.ts
@@ -4,7 +4,7 @@ function getRiddlesToRender(riddles: RiddleT[]) {
     const nextRiddleIndex = riddles.findIndex(r => !r.unlocked);
     const riddlesToRender = nextRiddleIndex === -1
         ? riddles
-        : riddles.slice(0, nextRiddleIndex + 1);
+        : riddles.slice(0, nextRiddleIndex);
 
     if (nextRiddleIndex !== -1) {
         riddlesToRender.push(riddles[nextRiddleIndex]);
