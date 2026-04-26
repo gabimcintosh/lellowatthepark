163985903aed41102f21f04eaba94b66442032a7
Trevor Durán <trevor.m.duran@gmail.com>
2026-04-26 16:44:21 -0500

    :hammer: chore: configure commitlint with TypeScript types

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,3 +1,15 @@
-const config = { extends: ["gitmoji"] };
+import { defineConfig } from "cz-git";
+
+const config = defineConfig({
+  extends: ["gitmoji"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+  },
+  prompt: {
+    // Enable Gitmoji in the cz-git prompt
+    useEmoji: true,
+    emojiAlign: "left",
+  },
+});
 
 export default config;
